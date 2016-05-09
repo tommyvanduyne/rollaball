950acb3c8545b831190367c6c39daa7b49156bf9
chadwickucla <chadwickucla@gmail.com>
2016-05-09 09:18:26 -0700

    added sphere and script at same time you added #test

---
+++ Assets/scripts/__Ideas.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+
+this script is strictly for not taking regarding ideas and what not. 
+
+
+
+
+
+
+*/
+
+
+public class __Ideas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
