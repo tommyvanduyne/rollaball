0be8944559cdbf6f50deceed29776c1cc3258a29
chadwickucla <chadwickucla@gmail.com>
2016-05-09 09:32:31 -0700

    Load between levels and added to each scene

---
+++ Assets/scripts/Level2Load.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Load : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider other){
+		Application.LoadLevel("minigameLevelTwo");
+	}
+}
