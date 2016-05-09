dc7c8783c7e8481610922abe8891835e4a8c7a10
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-09 10:48:25 -0400

    first commit

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    //Update called before rendering a frame
+    void FixedUpdate () //Called before performing and physics calculations
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
