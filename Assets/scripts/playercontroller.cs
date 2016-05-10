7cf6f752f04c294e2559dfe74a87ee31bbfc955b
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-10 12:16:09 -0400

    finished the game

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 //what up homie?
 public class playercontroller : MonoBehaviour {
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
     //Update called before rendering a frame
     void FixedUpdate () //Called before performing and physics calculations
@@ -17,5 +25,24 @@ public class playercontroller : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
